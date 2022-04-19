25ebdae44a427af303632827495c054f9feadca3
Rugy <Academiaunity50@gmail.com>
2022-04-19 16:10:57 +0200

    a

---
+++ Assets/_Script/Grenade.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    [Range(1,10)]
+    public float countdown = 3f;
+    [Range(5,20)]
+    public float radius = 5f;
+    [Range(500,1500)]
+    public float explosionForce = 700;
+
+    private bool hasExploded=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0 && !hasExploded)
+        {
+            Explode();
+            hasExploded = true;
+        }
+    }
+
+    void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+        foreach (var rangeObject in colliders)
+        {
+            Rigidbody rangeObjectRigidbody = rangeObject.GetComponent<Rigidbody>();
+            if (rangeObjectRigidbody != null)
+            {
+                rangeObjectRigidbody.AddExplosionForce(explosionForce,transform.position,radius);
+            }
+        }
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/_Script/ThrowGrenade.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowGrenade : MonoBehaviour
+{
+    public float throwForce = 500f;
+
+    public GameObject grenadePrefab;
+
+    public Transform spawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Throw();
+        }
+    }
+
+    void Throw()
+    {
+        GameObject newGrenade = Instantiate(grenadePrefab, spawnPoint.position, spawnPoint.rotation);
+        newGrenade.GetComponent<Rigidbody>().AddForce(spawnPoint.forward*throwForce,ForceMode.Impulse);
+    }
+}
